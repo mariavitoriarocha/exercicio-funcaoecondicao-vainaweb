6d9457fea92ce08adc6bd0054545d953ec875e91
mariavitoriarocha <maviestudando@gmail.com>
2023-01-27 23:47:13 -0300

    comentário

---
+++ script.js
@@ -0,0 +1,41 @@
+//atividade 1 - crie uma função que exiba uma mensagem no console//
+function mostrarNome1(nome) {
+    console.log(`Temos` + nome);
+    }
+    mostrarNome1(`água na geladeira`)
+
+//atividade 2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console//
+
+function mostrarNome2(nome) {
+    console.log(`Meu nome é ` + nome);
+    }
+    mostrarNome2(`Mavi`)
+
+//atividade 3 - crie uma função que receba nome, idade, e um estilo musical preferido (parâmetros) e exiba no console//
+
+function mostrarNome3(nome) {
+    console.log(nome);
+    }
+    mostrarNome3(`Lucas`)
+    mostrarNome3(`19 anos`)
+    mostrarNome3(`Pop`)
+
+//atividade 4 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console//
+
+function mostrarNome4(nome) {
+    console.log(nome);
+    }
+    mostrarNome4(`Avatar`)
+    mostrarNome4(`Breathin - Ariana Grande`)
+
+//atividade 5 - crie uma função que retorne o triplo do número recebido no parâmetro da função//
+
+function triple(num){
+    return num*3
+    }
+    console.log(triple(3))
+
+//atividade 6 - crie uma função que  verifique se uma  variável é true ou false//
+
+let number = 2
+console.log(number === "2")
